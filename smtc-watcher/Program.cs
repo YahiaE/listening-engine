7b121d0e377b932eb72c5d9b9aed641f94b0354b
YahiaE <yahiae2002@gmail.com>
2026-09-01 12:35:12 -0400

    Adjust reader to read generated UUID from Go program + Create a credential that can be referred back to the user ID using that UUID

--- smtc-watcher/Program.cs
+++ smtc-watcher/Program.cs
@@ -6,14 +6,18 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text;
+using Meziantou.Framework.Win32;
 
 public class Event {
+    public Guid? user_id {get; set;}
     public string song {get; set;}
     public string artist {get; set;}
     public string album {get; set;}
     public DateTime played_at {get; set;}
 
-    public Event(string s, string at, string am, DateTime pa){
+    public Event(Guid? ui, string s, string at, string am, DateTime pa){
+        user_id = ui;
         song = s;
         artist = at;
         album = am;
@@ -30,15 +34,35 @@ class Program {
     private static string? _lastTitle;
     private static string? _lastArtist;
     private static string? _lastAlbumTitle;
+    private static Windows.Media.Control.GlobalSystemMediaTransportControlsSessionPlaybackStatus? _lastStatus;
 
     private static readonly HttpClient client = new HttpClient();
-
+    private static readonly Windows.Security.Credentials.PasswordVault vault = new Windows.Security.Credentials.PasswordVault();
+    private static Windows.Security.Credentials.PasswordCredential credentials;
+    private static Guid userID;
     static async Task Main(){
         try {
+            credentials = vault.Retrieve(
+                "listening-engine-auth-token",
+                "user"
+            );
+            Console.WriteLine("Found existing user_id");
+            credentials.RetrievePassword();
+
+            var receivedID = credentials.Password;
+
+            userID = new Guid(receivedID);
+        } catch (Exception ex){
+            Console.WriteLine("Unable to find auth token");
+        }
+
+        try {
+            
             
             Console.WriteLine("Initializing Windows Media Session Manager...");
             _sessionManager = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
             Console.WriteLine("Session Manager Found!");
+             
 
             /* 
             event += event handler
@@ -99,15 +123,21 @@ class Program {
     }
 
     private static void OnPlaybackStateChanged(GlobalSystemMediaTransportControlsSession sender, PlaybackInfoChangedEventArgs args){
-        var playbackInfo = sender.GetPlaybackInfo();
-        Console.WriteLine(playbackInfo.PlaybackStatus);
+        var playbackInfo = sender.GetPlaybackInfo().PlaybackStatus;
+        if (_lastStatus != playbackInfo){
+            Console.WriteLine(playbackInfo);
+        }
+
+        _lastStatus = playbackInfo;
+
+        
     }
 
     private static async Task GrabMediaDataAsync(GlobalSystemMediaTransportControlsSession session){
         try {
             var props = await session.TryGetMediaPropertiesAsync();
 
-            if (props == null || (props.Title == _lastTitle && props.Artist == _lastArtist && props.AlbumTitle == _lastAlbumTitle)){
+            if (props == null || props.Title == "" || (props.Title == _lastTitle && props.Artist == _lastArtist && props.AlbumTitle == _lastAlbumTitle)){
                 return;
             }
         
@@ -115,22 +145,35 @@ class Program {
             _lastTitle = props.Title;
             _lastAlbumTitle = props.AlbumTitle;
             _lastArtist = props.Artist;
-            
-            
-            if (props.Title != "") {
-                Console.WriteLine(props.Title + " seen");
-                var newEvent = new Event(props.Title, props.AlbumTitle, props.Artist, DateTime.UtcNow);
-                HttpResponseMessage response = await client.PostAsJsonAsync("http://172.19.164.243:5000", newEvent);
-                response.EnsureSuccessStatusCode();
 
-                string responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine("Response received:");
-                Console.WriteLine(responseBody);
+            Event newEvent = null;
+
+            
+            
+            if (userID == null) {
+                newEvent = new Event(null, props.Title, props.AlbumTitle, props.Artist, DateTime.UtcNow);  
+            } else {
+                newEvent = new Event(userID, props.Title, props.AlbumTitle, props.Artist, DateTime.UtcNow);
+            }
+
+            HttpResponseMessage response = await client.PostAsJsonAsync("http://172.19.164.243:5000", newEvent);
+            response.EnsureSuccessStatusCode();
+
+            byte[] responseBody = await response.Content.ReadAsByteArrayAsync();
+
+            if (responseBody.Length > 0){
+                Guid decodedID = new Guid(responseBody, bigEndian: true);
+                credentials = new Windows.Security.Credentials.PasswordCredential(
+                    resource: "listening-engine-auth-token",
+                    userName: "user",
+                    password: decodedID.ToString());
+                vault.Add(credentials);
+                userID = decodedID;
             }
             
-
-           
+            Console.WriteLine(newEvent.user_id);
             
+
         } catch (Exception ex){
             Console.WriteLine($"Error: {ex.Message}");
         }
