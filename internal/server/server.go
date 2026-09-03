2fd5b6b7139a43631cc6beefe85fc2ad7b1cfe68
YahiaE <yahiae2002@gmail.com>
2026-09-03 18:19:30 -0400

    Import server elements from server branch

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,52 +4,80 @@ import (
 	"fmt"
 	"encoding/json"
 	"net/http"
+	"io"
 	"log"
 	"database/sql"
 	"github.com/YahiaE/listening-engine/internal/models"
+	"github.com/YahiaE/listening-engine/internal/auth"
 	"github.com/google/uuid"
 )
 
 var databasePool *sql.DB
-// var prevSong string
-// var prevArtist string
-// var prevAlbum string
+
+type Credential struct {
+	UserID string
+	Token string
+}
 
 func handler(w http.ResponseWriter, r *http.Request){
+	var credential Credential
 	var event models.ListeningEvent
+	
+	userToken := r.Header.Get("Auth-Token")
 
-	err := json.NewDecoder(r.Body).Decode(&event)
+	if len(userToken) == 0{
+		log.Println("Received empty token. Generating ID and token for user")
+		userUUID := uuid.New().String()
+		userToken, err := auth.GenerateToken();
+
+		if err != nil {
+			http.Error(w, "Internal server error: Unable to generate user credentials", http.StatusInternalServerError)
+			return
+		}
+
+		credential.UserID = userUUID
+		credential.Token = userToken
+
+		credentialsJson, err := json.Marshal(credential)
+
+		if err != nil {
+			http.Error(w, "Internal server error: Unable to format credentials", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(credentialsJson)		
+	}
+
+	event.UserID = credential.UserID
+	bodyBytes, err := io.ReadAll(r.Body)
 
 	if err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	err = json.Unmarshal(bodyBytes, &event)
+	if err != nil {
+		http.Error(w, "Internal server error: Unable to map data into usable object", http.StatusInternalServerError)
 		return
 	}
 
-<<<<<<< Updated upstream
-	w.WriteHeader(http.StatusOK)
-	if event.UserID == uuid.Nil {
-		event.UserID = uuid.New()
-		byteSlice := event.UserID[:]
-		log.Println(len(byteSlice))
-		w.Write(byteSlice)
-	}
-=======
-	// if (event.Song == prevSong && event.Artist == prevArtist && event.Album == prevAlbum){
-	// 	return
-	// }
-
-	// prevSong = event.Song
-	// prevArtist = event.Artist
-	// prevAlbum = event.Album
-
 	
 	log.Println(event.Song)
->>>>>>> Stashed changes
 
-	log.Println("Received body: ", event)
+
 
 	
+	
 }
+
+	
+
+	
+
 
 func Start(port string, db *sql.DB){
 	databasePool = db
@@ -57,7 +85,7 @@ func Start(port string, db *sql.DB){
 	fmt.Println(stats)
 	http.HandleFunc("/", handler)
 
-	fmt.Printf("Server is running on port %v", port)
+	fmt.Printf("Server is running on port %v\n", port)
 
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, nil))
